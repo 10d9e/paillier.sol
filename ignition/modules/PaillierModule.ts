dcc5b7858fda9297363fe1f6868da4f7c3b95a37
Jay Logelin <jay.logelin@gmail.com>
2024-05-04 11:22:13 -0300

    chore: cleanup

--- ignition/modules/PaillierModule.ts
+++ ignition/modules/PaillierModule.ts
@@ -1,8 +1,5 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
-
 const PaillierModule = buildModule("PaillierModule", (m) => {
   const paillier = m.contract("Paillier", []);
   return { paillier };
